7168e3708051060f716706b5af993b029d544fcb
Michal Staniecko <michal.staniecko@gmail.com>
2023-08-30 22:25:22 +0200

    rename

--- plugin.php
+++ plugin.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * Plugin Name:       Plugin Boilerplate
- * Description:       Plugin Boilerplate.
+ * Plugin Name:       Store plugin
+ * Description:       Store plugin.
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           0.1.0
  * Author:            The WordPress Contributors
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       plugin-boilerplate
+ * Text Domain:       store-plugin
  *
  * @package           create-block
  */
